44661a308ce90d8f793f939faaf35b5edac1ac37
fyhao <fyhao1@gmail.com>
2021-04-16 22:01:11 +0800

    init code

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+ 
+ // parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(12345)
